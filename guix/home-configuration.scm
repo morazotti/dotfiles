1485a591cc8db57f52ef34aec4bf288ee17c933c
morazotti <nicolas.morazotti@gmail.com>
2026-02-19 18:20:26 -0300

    Manage Emacs config via home-xdg-configuration-files
    * emacs: Bump submodule revision.
    * guix/home-configuration.scm: Generate XDG Emacs entries from item list.

--- guix/home-configuration.scm
+++ guix/home-configuration.scm
@@ -7,6 +7,32 @@
              (gnu packages version-control)
              (gnu packages password-utils))
 
+(define emacs-repo "/home/nicolas/repos/dotfiles/emacs")
+
+(define emacs-items
+  ;; (tipo . nome-relativo)
+  ;; tipo: 'file ou 'dir
+  '((file . "init.el")
+    (file . "early-init.el")
+    (file . "async-init.el")
+    (file . "abbrev_defs")
+    (file . "bookmarks")
+    (file . "emacs-custom.el")
+    (dir  . "macros")
+    (dir  . "snippets")
+    (dir  . "modules")))
+
+(define emacs-xdg-entries
+  (map (lambda (it)
+         (let ((kind (car it))
+               (path (cdr it)))
+           (list (string-append "emacs/" path)
+                 (if (eq? kind 'dir)
+                     (local-file (string-append emacs-repo "/" path) #:recursive? #t)
+                     (local-file (string-append emacs-repo "/" path))))))
+       emacs-items))
+
+
 (home-environment
  (packages
   (list zsh
@@ -15,12 +41,7 @@
         pass))
  (services
   (list
-   ;; Gerencie seu ~/.zshrc a partir da sua dotfile (em vez de gerar um zshrc).
    (service home-files-service-type
-            `((".zshrc" ,(local-file "/home/nicolas/repos/dotfiles/zsh/.zshrc"))
-              (".config/emacs/init.el" ,(local-file "/home/nicolas/repos/emacs/init.el"))
-              (".config/emacs/async-init.el" ,(local-file "/home/nicolas/repos/emacs/async-init.el"))
-              (".config/emacs/straight/versions"
-               ,(local-file "/home/nicolas/repos/emacs/straight/versions" #:recursive? #t))
-              (".config/emacs/early-init.el"
-               ,(local-file "/home/nicolas/repos/emacs/early-init.el")))))))
+            `((".zshrc" ,(local-file "/home/nicolas/repos/dotfiles/zsh/.zshrc"))))
+   (service home-xdg-configuration-files-service-type
+         emacs-xdg-entries))))
